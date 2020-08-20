6174f2fcf263738867db16f423e859d721ee414e
(youngho) <(one0374@naver.com)>
2020-08-20 21:09:04 +0900

    make to complete app-images.vue and should make router pages

--- src/view/app-images.vue
+++ src/view/app-images.vue
@@ -1,81 +1,141 @@
 <template>
   <div class="images-container">
-    <ul id="before-high-school-container" class="box">
-      <span class="subject-block">고등학교 이전</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/beforeHighSchool/IMG_20121215_195537.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/beforeHighSchool/IMG_20121215_184708.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/beforeHighSchool/IMG_20121110_151415.jpg" alt />
-      </li>
-    </ul>
-    <ul id="high-school-container" class="box">
-      <span class="subject-block">고등학교</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/highSchool/IMG_20160508_162754.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/highSchool/IMG_20160508_162246.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/highSchool/IMG_20160508_140349.jpg" alt />
-      </li>
-    </ul>
-    <ul id="2017year-container" class="box">
-      <span class="subject-block">2017년도</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/2017/1485881647583.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/2017/1486058438813.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/2017/IMG_20170204_075554.jpg" alt />
-      </li>
-    </ul>
-    <ul id="2018year-container" class="box">
-      <span class="subject-block">2018년도</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/2018/1519200122270.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/2018/20180220_153742.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/2018/1519029953352.jpg" alt />
-      </li>
-    </ul>
-    <ul id="2019year-container" class="box">
-      <span class="subject-block">2019년도</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/2019/20190211_152534.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/2019/20190211_152600.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/2019/20190211_210859.jpg" alt />
-      </li>
-    </ul>
-    <ul id="2020year-container" class="box">
-      <span class="subject-block">2020년도</span>
-      <li class="image-box" id="box1">
-        <img src="../assets/images/2020/전역사진.jpg" alt />
-      </li>
-      <li class="image-box" id="box2">
-        <img src="../assets/images/2020/액자.jpg" alt />
-      </li>
-      <li class="image-box" id="box3">
-        <img src="../assets/images/2020/술자리.jpg" alt />
-      </li>
-    </ul>
+    <router-link to="/">
+      <ul
+        id="before-high-school-container"
+        class="box"
+        v-on:mouseover="flag1=false"
+        @mouseout="flag1=true"
+        :key="images[0]"
+      >
+        <span class="subject-block">고등학교 이전</span>
+        <li class="image-box box1">
+          <img src="../assets/images/beforeHighSchool/IMG_20121215_195537.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag1, box2:flag1}">
+          <img src="../assets/images/beforeHighSchool/IMG_20121215_184708.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag1, box3:flag1}">
+          <img src="../assets/images/beforeHighSchool/IMG_20121110_151415.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
+    <router-link to="/">
+      <ul
+        id="high-school-container"
+        class="box"
+        v-on:mouseover="flag2=false"
+        @mouseout="flag2=true"
+        :key="images[1]"
+      >
+        <span class="subject-block">고등학교</span>
+        <li class="image-box box1">
+          <img src="../assets/images/highSchool/IMG_20160508_162754.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag2, box2:flag2}">
+          <img src="../assets/images/highSchool/IMG_20160508_162246.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag2, box3:flag2}">
+          <img src="../assets/images/highSchool/IMG_20160508_140349.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
+    <router-link to="/">
+      <ul
+        id="2017year-container"
+        class="box"
+        v-on:mouseover="flag3=false"
+        @mouseout="flag3=true"
+        :key="images[2]"
+      >
+        <span class="subject-block">2017년도</span>
+        <li class="image-box box1">
+          <img src="../assets/images/2017/1485881647583.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag3, box2:flag3}">
+          <img src="../assets/images/2017/1486058438813.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag3, box3:flag3}">
+          <img src="../assets/images/2017/IMG_20170204_075554.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
+    <router-link to="/">
+      <ul
+        id="2018year-container"
+        class="box"
+        v-on:mouseover="flag4=false"
+        @mouseout="flag4=true"
+        :key="images[3]"
+      >
+        <span class="subject-block">2018년도</span>
+        <li class="image-box box1">
+          <img src="../assets/images/2018/1519200122270.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag4, box2:flag4}">
+          <img src="../assets/images/2018/20180220_153742.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag4, box3:flag4}">
+          <img src="../assets/images/2018/1519029953352.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
+    <router-link to="/">
+      <ul
+        id="2019year-container"
+        class="box"
+        v-on:mouseover="flag5=false"
+        @mouseout="flag5=true"
+        :key="images[4]"
+      >
+        <span class="subject-block">2019년도</span>
+        <li class="image-box box1">
+          <img src="../assets/images/2019/20190211_152534.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag5, box2:flag5}">
+          <img src="../assets/images/2019/20190211_152600.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag5, box3:flag5}">
+          <img src="../assets/images/2019/20190211_210859.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
+    <router-link to="/">
+      <ul
+        id="2020year-container"
+        class="box"
+        v-on:mouseover="flag6=false"
+        @mouseout="flag6=true"
+        :key="images[5]"
+      >
+        <span class="subject-block">2020년도</span>
+        <li class="image-box box1">
+          <img src="../assets/images/2020/전역사진.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement1: !flag6, box2:flag6}">
+          <img src="../assets/images/2020/액자.jpg" alt />
+        </li>
+        <li class="image-box" :class="{movement2: !flag6, box3:flag6}">
+          <img src="../assets/images/2020/술자리.jpg" alt />
+        </li>
+      </ul>
+    </router-link>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      images: [0, 1, 2, 3, 4, 5],
+      flag1: true,
+      flag2: true,
+      flag3: true,
+      flag4: true,
+      flag5: true,
+      flag6: true,
+    };
+  },
+};
 </script>
 <style scoped>
 :root {
@@ -97,15 +157,18 @@ export default {};
   --dark-main-color: #3e2723;
   --light-main-color: #d7ccc8;
 }
+/* 사진들 옮기게 해줄 class선택자 */
 .images-container {
   height: 1296px;
   margin: var(--regular-spacing);
   border: 1.5px solid black;
   border-radius: 5px;
+  overflow-x: scroll;
 }
 
 /* 모든 앨범들 공통*/
 .images-container .box {
+  width: 200px;
   position: relative;
   height: 200px;
   padding: 0;
@@ -116,15 +179,15 @@ export default {};
   display: inline-block;
   margin: 0 0 var(--small-spacing) var(--big-spacing);
 }
-.images-container .box #box1 {
+.images-container .box .box1 {
   left: 0px;
   z-index: 100;
 }
-.images-container .box #box2 {
+.images-container .box .box2 {
   left: 20px;
   z-index: 90;
 }
-.images-container .box #box3 {
+.images-container .box .box3 {
   left: 40px;
   z-index: 80;
 }
@@ -137,5 +200,11 @@ export default {};
 .images-container .box .image-box img {
   width: 100%;
   height: 100%;
+}
+.images-container .box .movement1 {
+  left: 170px;
+}
+.images-container .box .movement2 {
+  left: 340px;
 }
 </style>
